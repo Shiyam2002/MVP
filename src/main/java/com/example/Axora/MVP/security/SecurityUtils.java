4a76572cb0357664d0c74f15a83c9f0b9f791289
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2025-12-25 20:31:25 +0530

    feat: updated missing annotation in the SecurityUtils

--- src/main/java/com/example/Axora/MVP/security/SecurityUtils.java
+++ src/main/java/com/example/Axora/MVP/security/SecurityUtils.java
@@ -2,10 +2,12 @@ package com.example.Axora.MVP.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.UUID;
 
+@Component
 public class SecurityUtils {
 
     public static UUID getCurrentUser() {
